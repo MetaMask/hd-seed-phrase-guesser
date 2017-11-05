42530879e9a6e91daf7614f6f04384de162670d6
Dan Finlay <dan@danfinlay.com>
2017-11-04 22:07:43 -0700

    First commit
    Can trim stray spaces from a seed phrase

---
+++ index.js
@@ -0,0 +1,58 @@
+const spell = require('spell')
+const EventEmitter = require('events')
+const HdKeyring = require('eth-hd-keyring')
+const words = require('bip39/wordlists/english.json')
+const dict = spell()
+
+dict.load(words.join(' '))
+
+class Guesser extends EventEmitter {
+
+  constructor(opts) {
+    super()
+    const { phrase, getBalance } = opts
+    this.phrase = phrase
+    this.getBalance = getBalance
+
+    this.cleanup()
+  }
+
+  cleanup () {
+    this.phrase = this.phrase.split(' ')
+    .filter((word) => {
+      return word !== ''
+    })
+    .join(' ')
+  }
+
+  async guess (depth = 5) {
+    this.depth = depth
+
+    if (this.result) {
+      return this.emit('found', this.result)
+    }
+
+    console.log('trying with ' + this.phrase)
+    const keyring = new HdKeyring({
+      mnemonic: this.phrase,
+      numberOfAccounts: this.depth,
+    })
+
+    const accounts = await keyring.getAccounts()
+    let balances = await Promise.all(accounts.map((account) => {
+      return this.getBalance(account)
+    }))
+    balances = balances.filter(balance => balance.toString() !== '0')
+
+    if (balances.length > 0) {
+      this.result = this.phrase
+      return this.emit('found', this.result)
+    }
+
+    return this.emit('gave-up')
+  }
+
+}
+
+module.exports = Guesser
+
---
+++ test/index.js
@@ -0,0 +1,33 @@
+const test = require('tape')
+const Guesser = require('../')
+const BN = require('ethereumjs-util').BN
+
+const phrase = 'travel funny ocean erupt crater whale mandate timber scatter scout actress elbow'
+
+test('can fix a space at the beginning of a phrase', function (t) {
+  t.plan(1)
+
+  const input = ' ' + phrase
+  const expected = '0xf40762669ac55878e34691e6fc048e75518043c9'
+  const getBalance = async (addr) => {
+
+    if (addr.toLowerCase() === expected) {
+      return new BN(10000000000)
+    } else {
+      return new BN(0)
+    }
+  }
+
+  const guesser = new Guesser({ phrase: input, getBalance })
+
+  guesser.on('found', (result) => {
+    t.equal(result, phrase, 'found the correct phrase')
+  })
+
+  guesser.on('gave-up', () => {
+    t.fail('gave up')
+  })
+
+  guesser.guess()
+})
+
